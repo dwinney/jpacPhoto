6d9cc4ed41f7b46c6518a6a1cc1e72906a4c73d3
Daniel Winney <dwinney@iu.edu>
2021-03-31 23:31:15 -0400

    All the correct bounds on t and M2

--- include/inclusive/inclusive_kinematics.hpp
+++ include/inclusive/inclusive_kinematics.hpp
@@ -44,45 +44,83 @@ namespace jpacPhoto
         // ---------------------------------------------------------------------------
         // Center-of-mass kinematics
 
-        inline double M2(double s, double x, double pT2)
+        // Momenta
+        inline double pX(double s, double M2)
+        {
+            return sqrt(Kallen(s, _mX2, M2)) / (2. * sqrt(s));
+        };
+
+        inline double pX_max(double s)
+        {
+            return sqrt(Kallen(s, _mX2, _minM2)) / (2. * sqrt(s));
+        };
+
+        inline double pGamma(double s)
+        {
+            return sqrt(Kallen(s, 0., M2_PROTON)) / (2. * sqrt(s));
+        };
+
+        // Kinematic bounds for t and M2 with the other fixed
+        inline double t_bounds(int pm, double s, double M2)
+        {
+            double result; 
+            result  = _mX2 - (s - _mT2) * (s - M2 + _mX2) / (2. * s);
+            result += double(pm) * 2. * pX(s, M2) * pGamma(s);
+
+            return result;
+        };
+
+        inline double t_bounds(int pm, double s)
+        {
+            return (-_minM2*_mT2 + _mT2*_mX2 + (_minM2+_mT2+_mX2)*s-s*s-double(pm)*(s-_mT2)*sqrt(_minM2+pow(_mX2 - s, 2.)-2.*_minM2*(_mX2 + s))) /2./s;
+        };
+
+        inline double M2_bounds(int pm, double s, double t)
+        {
+            if (pm == -1)
+            {
+                return _minM2;
+            } 
+            else 
+            {
+                return (_mT2 + _mX2 - s - t) * (_mT2*_mX2 - s*t) / (_mT2 - s) / (_mX2 - t);
+            }
+        };
+
+        inline double M2_bounds(int pm, double s)
+        {
+            if (pm == -1) return _minM2;
+            else return pow((sqrt(s) - _mX), 2.);
+        };
+
+        // ---------------------------------------------------------------------------
+        // Starting from (x, pT2) -> (t, M2)
+
+        inline double M2_from_xpT2(double s, double x, double pT2)
         {
             double pPara = x * pX_max(s);
             double EX    = sqrt(_mX2 + pT2 + pPara*pPara);
 
-            return s + _mX2 - _minM2 - 2.*sqrt(s)*EX;
+            return s + _mX2 - 2.*sqrt(s)*EX;
         };
 
-        inline double cosTheta(double s, double x, double pT2)
+        inline double cosTheta_from_xpT2(double s, double x, double pT2)
         {
-            return x * pX_max(s) / pX_CM(s, M2(s, x, pT2));
+            double M2 = M2_from_xpT2(s, x, pT2);
+            return x * pX_max(s) / pX(s, M2);
         };
 
-        inline double t_man(double s, double x, double pT2)
+        inline double t_from_xpT2(double s, double x, double pT2)
         {
-            double m2       = M2(s, x, pT2);
-            double CosTheta = cosTheta(s, x, pT2); 
-
-            double pGamma = sqrt(Kallen(s, 0., M2_PROTON)) / (2. * sqrt(s));
-            double pX     = pX_CM(s, m2);
-
-            double sigma = _mT2 + _mX2 + m2;
+            double M2       = M2_from_xpT2(s, x, pT2);
+            double cosTheta = cosTheta_from_xpT2(s, x, pT2); 
+            double sigma = _mT2 + _mX2 + M2;
 
             double result;
-            result  = 2. * pGamma * pX * CosTheta;
-            result += (sigma - s) / 2. + _mT2*(_mX2 - m2)/(2.*s);
+            result  = 2. * pGamma(s) * pX(s, M2) * cosTheta;
+            result += (sigma - s) / 2. + _mT2*(_mX2 - M2)/(2.*s);
             
             return result;
-        };
-
-
-        inline double pX_CM(double xs, double xM2)
-        {
-            return sqrt(Kallen(xs, _mX2, xM2)) / (2. * sqrt(xs));
-        };
-
-        inline double pX_max(double xs)
-        {
-            return sqrt(Kallen(xs, _mX2, _minM2)) / (2. * sqrt(xs));
         };
     };
 };
