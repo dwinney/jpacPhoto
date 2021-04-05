ae4ddfab7cc45c99bb847c5fdef8d050f8a88285
Daniel Winney <dwinney@iu.edu>
2021-04-05 04:34:21 -0400

    added lambda_c exchanges

--- executables/open_charm/loop_amp.cpp
+++ executables/open_charm/loop_amp.cpp
@@ -14,10 +14,11 @@
 #include "helicities.hpp"
 #include "amplitudes/reaction_kinematics.hpp"
 #include "amplitudes/vector_exchange.hpp"
+#include "amplitudes/dirac_exchange.hpp"
 #include "amplitudes/pomeron_exchange.hpp"
 #include "amplitudes/amplitude_sum.hpp"
 #include "one_loop/box_amplitude.hpp"
-
+#include "photoPlotter.hpp"
 #include <cstring>
 #include <iostream>
 #include <iomanip>
@@ -34,16 +35,7 @@ int main( int argc, char** argv )
     // D phototproduction
     // ---------------------------------------------------------------------------
 
-    // Set up Kinematics for the overall process gamma p -> jpsi p
-    auto kPsi = new reaction_kinematics(M_JPSI, M_PROTON);
-    kPsi->set_JP(1, -1); // Vector production
 
-    // Kinematics for the sub-processes (gamma / psi) p -> Lam D
-    auto kgamD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON);
-    kgamD->set_JP(0, -1);  // Pseudo-scalar production
-
-    auto kpsiD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON, M_JPSI);
-    kpsiD->set_JP(0, -1);  // Pseudo-scalar production
 
     // Couplings
     double lambdaQCD  = 0.250, e = sqrt(4. * PI * ALPHA);
@@ -51,62 +43,75 @@ int main( int argc, char** argv )
     double gDNLam     = -4.3,  gDstarNLam     = -13.2;
     double gPsiDD     = 7.44;
     double gPsiDDstar = gPsiDD / sqrt(M_D * M_DSTAR);
+    double gPsiLamLam = -1.4;
 
-    // Vector exchange loop
+    // ---------------------------------------------------------------------------
+    // Gamma amplitudes
+
+    // Kinematics for the sub-processes (gamma) p -> Lam D
+    auto kgamD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON);
+    kgamD->set_JP(0, -1);  // Pseudo-scalar production
     auto gamDDstarEx = new vector_exchange(kgamD, M_DSTAR); 
     gamDDstarEx->set_params({gGamDDstar, gDstarNLam, 0.});
     gamDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
+
+    auto gamDLamEx = new dirac_exchange(kgamD, M_LAMBDAC); 
+    gamDLamEx->set_params({e, gDNLam});
+    gamDLamEx->set_formfactor(2, M_LAMBDAC + lambdaQCD * eta);
+
+    // ---------------------------------------------------------------------------
+    // Psi amplitudes
+
+    auto kpsiD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON, M_JPSI);
+    kpsiD->set_JP(0, -1);  // Pseudo-scalar production
 
     auto psiDDstarEx = new vector_exchange(kpsiD, M_DSTAR);
     psiDDstarEx->set_params({gPsiDDstar, gDstarNLam, 0.});
     psiDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
 
+    auto psiDLamEx = new dirac_exchange(kpsiD, M_LAMBDAC); 
+    psiDLamEx->set_params({gPsiLamLam, gDNLam});
+    psiDLamEx->set_formfactor(2, M_LAMBDAC + lambdaQCD * eta);
+
+    // ---------------------------------------------------------------------------
+    // Sums amplitudes
+
+    auto gamSum = new amplitude_sum(kgamD, {gamDDstarEx, gamDLamEx});
+    auto psiSum = new amplitude_sum(kpsiD, {psiDDstarEx, psiDLamEx});
+
+    // ---------------------------------------------------------------------------
+    // Box Amplitude
+
+    // Set up Kinematics for the overall process gamma p -> jpsi p
+    auto kJPsi = new reaction_kinematics(M_JPSI, M_PROTON);
+    kJPsi->set_JP(1, -1); // Vector production
+
     // Combine sub-processes in a box_loop
-    auto disc = new box_discontinuity(gamDDstarEx, psiDDstarEx);
-    auto ddstar_box = new box_amplitude(kPsi, disc, "Charm Loop");
+    auto disc = new box_discontinuity(gamSum, psiSum);
+    auto dBox = new box_amplitude(kJPsi, disc, "Charm Loop");
 
     double W_cut = sqrt(qmax*qmax + M2_LAMBDAC) + sqrt(qmax*qmax + M2_D);
-    ddstar_box->set_cutoff(W_cut * W_cut);
+    dBox->set_cutoff(W_cut * W_cut);
 
     // ---------------------------------------------------------------------------
-    // Plotting options
-    // ---------------------------------------------------------------------------
-
-    auto alpha = new linear_trajectory(+1, 0.941, 0.364);
-    auto pomeron = new pomeron_exchange(kPsi, alpha, 0, "Pomeron");
-    pomeron->set_params({0.379, 0.12});
-
-    // ---------------------------------------------------------------------------
-    // Plotting options
-    // ---------------------------------------------------------------------------
-
-    // which amps to plot
-    std::vector<amplitude*> amps;
-    amps.push_back(ddstar_box);
-    amps.push_back(pomeron);
-
-        // ---------------------------------------------------------------------------
     // You shouldnt need to change anything below this line
     // ---------------------------------------------------------------------------
     
     int N = 10;
 
+    double Egam = 8.7;
     double xmin = 0.;
     double xmax = 90.;
 
-    double theta = 0.;
-
    // ---------------------------------------------------------------------------
     // Print the desired observable for each amplitude
-    std::cout << std::endl << "Printing amplitude: " << amps[n]->_identifier << "\n";
 
     auto F = [&](double x)
     {
-        double W = W_cm(8.7);
+        double W = W_cm(Egam);
         double s = W*W;
-        double t = kPsi->t_man(s, x * DEG2RAD);
-        
-        return amps[n]->beam_asymmetry_4pi(s, t);
+        double t = kJPsi->t_man(s, x * DEG2RAD);
+        return dBox->beam_asymmetry_4pi(s, t);
     };
 
     std::array<std::vector<double>, 2> x_fx;
