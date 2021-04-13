171ed6af438d00032c27e6e3011c774238a920de
Daniel Winney <dwinney@iu.edu>
2021-04-13 15:35:24 -0400

    updated testing executables

--- executables/open_charm/jpsi_p.cpp
+++ executables/open_charm/jpsi_p.cpp
@@ -16,43 +16,50 @@ using namespace jpacPhoto;
 
 int main( int argc, char** argv )
 {
-    // Form factor parameter
-    double eta = 1.;
+    double eta       = 1.,    lambdaQCD      = 0.25;  // Form factor parameters
+    double gPsiDD    = 7.44;                          // Top couplings
+    double gPsiDDstar = gPsiDD / sqrt(M_D * M_DSTAR);
+    double gDNLam    = -4.3,  gDstarNLam     = -13.2; // Bottom couplings
 
     // ---------------------------------------------------------------------------
     // D phototproduction
     // ---------------------------------------------------------------------------
 
     // Set up Kinematics for Dbar LambdaC in final state
-    auto kD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON, M_JPSI);
-    kD->set_JP(PSEUDO_SCALAR);
+    auto kD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON);
+    kD->set_JP(VECTOR);
 
-    auto d_dstarEx = new vector_exchange(kD, M_DSTAR, "FF");
-    d_dstarEx->set_params({0.001, -13.2, 0.});
-    d_dstarEx->set_formfactor(2, M_DSTAR + eta * 0.250);
+    auto d_dEx = new pseudoscalar_exchange(kD, M_D, "Analytic");
+    d_dEx->set_params({gPsiDD, gDNLam});
+    d_dEx->set_formfactor(2, M_D + eta * lambdaQCD);
 
-    auto d_dstarExC = new vector_exchange(kD, M_DSTAR, "no FF");
-    d_dstarExC->set_params({0.01, -13.2, 0.});
-    d_dstarExC->set_formfactor(0, M_DSTAR + eta * 0.250);
-    // d_dstarExC->set_debug(1);
+    auto d_dExC = new pseudoscalar_exchange(kD, M_D, "Covariant");
+    d_dExC->set_params({gPsiDD, gDNLam});
+    d_dExC->set_formfactor(2, M_D + eta * lambdaQCD);
+    d_dExC->set_debug(1);
+
+    // auto d_dstarEx = new vector_exchange(kD, M_DSTAR, "D* exchange");
+    // d_dstarEx->set_params({gPsiDDstar, gDstarNLam, 0.});
+    // d_dstarEx->set_formfactor(2, M_DSTAR + eta * lambdaQCD);
 
    // ---------------------------------------------------------------------------
     // which amps to plot
     std::vector<amplitude*> amps;
-    amps.push_back(d_dstarEx);
-    amps.push_back(d_dstarExC);
+    amps.push_back(d_dEx);
+    amps.push_back(d_dExC);
+    // amps.push_back(d_dstarEx);
 
     auto plotter = new photoPlotter(amps);
 
     plotter->N = 30;
     plotter->PRINT_TO_COMMANDLINE = true;
-    plotter->LAB_ENERGY = false;
+    plotter->LAB_ENERGY = true;
 
     plotter->xmin = 4.;
     plotter->xmax = 10.5;
 
     plotter->ymin = 0.;
-    plotter->ymax = 540.;
+    plotter->ymax = 10000.;
 
     plotter->SHOW_LEGEND = true;
     plotter->xlegend = 0.2;
--- executables/open_charm/loop_amp.cpp
+++ executables/open_charm/loop_amp.cpp
@@ -18,7 +18,9 @@
 #include "amplitudes/pomeron_exchange.hpp"
 #include "amplitudes/amplitude_sum.hpp"
 #include "one_loop/box_amplitude.hpp"
-#include "photoPlotter.hpp"
+
+#include "jpacGraph1D.hpp"
+
 #include <cstring>
 #include <iostream>
 #include <iomanip>
@@ -27,15 +29,10 @@ using namespace jpacPhoto;
 
 int main( int argc, char** argv )
 {
-    // Form factor parameter
-    double eta  = 1.;
-    double qmax = 1.;
 
     // ---------------------------------------------------------------------------
-    // D phototproduction
+    // D loop
     // ---------------------------------------------------------------------------
-
-
 
     // Couplings
     double lambdaQCD  = 0.250, e = sqrt(4. * PI * ALPHA);
@@ -51,13 +48,9 @@ int main( int argc, char** argv )
     // Kinematics for the sub-processes (gamma) p -> Lam D
     auto kgamD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON);
     kgamD->set_JP(0, -1);  // Pseudo-scalar production
+    
     auto gamDDstarEx = new vector_exchange(kgamD, M_DSTAR); 
     gamDDstarEx->set_params({gGamDDstar, gDstarNLam, 0.});
-    gamDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
-
-    auto gamDLamEx = new dirac_exchange(kgamD, M_LAMBDAC); 
-    gamDLamEx->set_params({e, gDNLam});
-    gamDLamEx->set_formfactor(2, M_LAMBDAC + lambdaQCD * eta);
 
     // ---------------------------------------------------------------------------
     // Psi amplitudes
@@ -67,17 +60,6 @@ int main( int argc, char** argv )
 
     auto psiDDstarEx = new vector_exchange(kpsiD, M_DSTAR);
     psiDDstarEx->set_params({gPsiDDstar, gDstarNLam, 0.});
-    psiDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
-
-    auto psiDLamEx = new dirac_exchange(kpsiD, M_LAMBDAC); 
-    psiDLamEx->set_params({gPsiLamLam, gDNLam});
-    psiDLamEx->set_formfactor(2, M_LAMBDAC + lambdaQCD * eta);
-
-    // ---------------------------------------------------------------------------
-    // Sums amplitudes
-
-    auto gamSum = new amplitude_sum(kgamD, {gamDDstarEx, gamDLamEx});
-    auto psiSum = new amplitude_sum(kpsiD, {psiDDstarEx, psiDLamEx});
 
     // ---------------------------------------------------------------------------
     // Box Amplitude
@@ -87,11 +69,23 @@ int main( int argc, char** argv )
     kJPsi->set_JP(1, -1); // Vector production
 
     // Combine sub-processes in a box_loop
-    auto disc = new box_discontinuity(gamSum, psiSum);
+    auto disc = new box_discontinuity(gamDDstarEx, psiDDstarEx);
     auto dBox = new box_amplitude(kJPsi, disc, "Charm Loop");
 
-    double W_cut = sqrt(qmax*qmax + M2_LAMBDAC) + sqrt(qmax*qmax + M2_D);
-    dBox->set_cutoff(W_cut * W_cut);
+    auto W_cut = [&](double qmax)
+    { 
+        return sqrt(qmax*qmax + M2_LAMBDAC) + sqrt(qmax*qmax + M2_D);
+    };
+
+    double qmax = 1.;
+    dBox->set_cutoff(W_cut(qmax) * W_cut(qmax));
+
+    double eta  = 1.;
+    gamDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
+    psiDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
+
+    gamDDstarEx->set_debug(2);
+    psiDDstarEx->set_debug(2);
 
     // ---------------------------------------------------------------------------
     // You shouldnt need to change anything below this line
@@ -99,23 +93,39 @@ int main( int argc, char** argv )
     
     int N = 10;
 
-    double Egam = 8.7;
-    double xmin = 0.;
-    double xmax = 90.;
-
    // ---------------------------------------------------------------------------
     // Print the desired observable for each amplitude
+    jpacGraph1D * plotter = new jpacGraph1D();
 
-    auto F = [&](double x)
+
+    double xmin = E_beam(kJPsi->Wth()) + EPS;
+    double xmax = 9.2;
+
+    auto F = [&](double egam)
     {
-        double W = W_cm(Egam);
-        double s = W*W;
-        double t = kJPsi->t_man(s, x * DEG2RAD);
-        return dBox->beam_asymmetry_4pi(s, t);
+        double s = pow(W_cm(egam), 2.);
+        return dBox->integrated_xsection(s);
     };
+    
+    auto x_fx = vec_fill(N, F, xmin, xmax, true);
+    plotter->AddEntry(std::get<0>(x_fx), std::get<1>(x_fx), "Du et al.");
 
-    std::array<std::vector<double>, 2> x_fx;
-    x_fx = vec_fill(N, F, xmin, xmax, true);
+    plotter->SetLegend(0.2, 0.6);
+    plotter->SetLegendOffset(0.2, 0.1);
+    plotter->SetLegend(false);
+
+    plotter->SetYaxis("#sigma(#gamma p #rightarrow J/#psi p)    [nb]", 0., 1.);
+    plotter->SetXaxis("E_{#gamma}    [GeV]", xmin, xmax);
+    plotter->Plot("helamp.pdf");
+
+    delete plotter;
+    delete kJPsi;
+    delete kgamD;
+    delete kpsiD;
+    delete disc;
+    delete dBox;
+    delete psiDDstarEx;
+    delete gamDDstarEx;
 
     return 1;
 };
