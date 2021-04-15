39bb693fe69bbf85536ff288fdf97574271285a3
Daniel Winney <dwinney@iu.edu>
2021-04-14 23:58:53 -0400

    full final loop_amp!

--- executables/open_charm/loop_amp.cpp
+++ executables/open_charm/loop_amp.cpp
@@ -11,10 +11,9 @@
 // ---------------------------------------------------------------------------
 
 #include "constants.hpp"
-#include "helicities.hpp"
 #include "amplitudes/reaction_kinematics.hpp"
 #include "amplitudes/vector_exchange.hpp"
-#include "amplitudes/dirac_exchange.hpp"
+#include "amplitudes/pseudoscalar_exchange.hpp"
 #include "amplitudes/pomeron_exchange.hpp"
 #include "amplitudes/amplitude_sum.hpp"
 #include "one_loop/box_amplitude.hpp"
@@ -30,17 +29,30 @@ using namespace jpacPhoto;
 int main( int argc, char** argv )
 {
 
-    // ---------------------------------------------------------------------------
-    // D loop
-    // ---------------------------------------------------------------------------
+    double eta  = 1.;
+    double qmax = 1.;
+    auto s_cut = [&](double qmax, double m)
+    { 
+        double W_cut = sqrt(qmax*qmax + M2_LAMBDAC) + sqrt(qmax*qmax + m*m);
+        return W_cut*W_cut;
+    };
 
     // Couplings
     double lambdaQCD  = 0.250, e = sqrt(4. * PI * ALPHA);
     double gGamDDstar = 0.134, gGamDstarDstar = 0.641;
     double gDNLam     = -4.3,  gDstarNLam     = -13.2;
+    double gPsiLamLam = -1.4;
     double gPsiDD     = 7.44;
     double gPsiDDstar = gPsiDD / sqrt(M_D * M_DSTAR);
-    double gPsiLamLam = -1.4;
+    double gPsiDstarDstar = gPsiDD * (M_DSTAR / M_D);
+
+    // Set up Kinematics for the overall process gamma p -> jpsi p
+    auto kJPsi = new reaction_kinematics(M_JPSI, M_PROTON);
+    kJPsi->set_JP(1, -1); // Vector production
+
+    // ---------------------------------------------------------------------------
+    // D loop
+    // ---------------------------------------------------------------------------
 
     // ---------------------------------------------------------------------------
     // Gamma amplitudes
@@ -49,8 +61,9 @@ int main( int argc, char** argv )
     auto kgamD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON);
     kgamD->set_JP(0, -1);  // Pseudo-scalar production
     
-    auto gamDDstarEx = new vector_exchange(kgamD, M_DSTAR); 
-    gamDDstarEx->set_params({gGamDDstar, gDstarNLam, 0.});
+    auto gamD_DstarEx = new vector_exchange(kgamD, M_DSTAR, "D* exchange"); 
+    gamD_DstarEx->set_params({gGamDDstar, gDstarNLam, 0.});
+    gamD_DstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
 
     // ---------------------------------------------------------------------------
     // Psi amplitudes
@@ -58,74 +71,111 @@ int main( int argc, char** argv )
     auto kpsiD = new reaction_kinematics(M_D, M_LAMBDAC, M_PROTON, M_JPSI);
     kpsiD->set_JP(0, -1);  // Pseudo-scalar production
 
-    auto psiDDstarEx = new vector_exchange(kpsiD, M_DSTAR);
-    psiDDstarEx->set_params({gPsiDDstar, gDstarNLam, 0.});
+    auto psiD_DEx = new pseudoscalar_exchange(kpsiD, M_D, "D exchange");
+    psiD_DEx->set_params({gPsiDD, gDNLam});
+    psiD_DEx->set_formfactor(2, M_D + lambdaQCD * eta);
+
+    auto psiD_DstarEx = new vector_exchange(kpsiD, M_DSTAR, "D* exchange");
+    psiD_DstarEx->set_params({gPsiDDstar, gDstarNLam, 0.});
+    psiD_DstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
+
+    auto psiD_Sum = new amplitude_sum(kpsiD, {psiD_DEx, psiD_DstarEx});
 
     // ---------------------------------------------------------------------------
     // Box Amplitude
 
-    // Set up Kinematics for the overall process gamma p -> jpsi p
-    auto kJPsi = new reaction_kinematics(M_JPSI, M_PROTON);
-    kJPsi->set_JP(1, -1); // Vector production
+    // Combine sub-processes in a box_loop
+    auto dDisc = new box_discontinuity(gamD_DstarEx, psiD_Sum);
+    auto dBox  = new box_amplitude(kJPsi, dDisc, "D Loop");
+    dBox->set_cutoff(s_cut(qmax, M_D));
+
+    // ---------------------------------------------------------------------------
+    // Dstar loop
+    // ---------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------
+    // Gamma amplitudes
+
+    // Kinematics for the sub-processes gamma p -> Lam Dstar
+    auto kgamDstar = new reaction_kinematics(M_DSTAR, M_LAMBDAC, M_PROTON);
+    kgamDstar->set_JP(1, -1);  // Vector 
+    
+    auto gamDstar_DEx = new pseudoscalar_exchange(kgamDstar, M_D);
+    gamDstar_DEx->set_params({gGamDDstar, gDstarNLam});
+    gamDstar_DEx->set_formfactor(2, M_D + lambdaQCD * eta);
+
+    auto gamDstar_DstarEx = new vector_exchange(kgamDstar, M_DSTAR); 
+    gamDstar_DstarEx->set_params({gGamDstarDstar, gDstarNLam, 0.});
+    gamDstar_DstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
+
+    auto gamDstar_Sum = new amplitude_sum(kgamDstar, {gamDstar_DEx, gamDstar_DstarEx});
+
+    // ---------------------------------------------------------------------------
+    // Psi amplitudes
+
+    // Kinematics for the sub-processes psi p -> Lam Dstar
+    auto kpsiDstar = new reaction_kinematics(M_DSTAR, M_LAMBDAC, M_PROTON, M_JPSI);
+    kpsiDstar->set_JP(1, -1); 
+
+    auto psiDstar_DEx = new pseudoscalar_exchange(kpsiDstar, M_D);
+    psiDstar_DEx->set_params({gPsiDDstar, gDstarNLam});
+    psiDstar_DEx->set_formfactor(2, M_D + lambdaQCD * eta);
+
+    auto psiDstar_DstarEx = new vector_exchange(kpsiDstar, M_DSTAR); 
+    psiDstar_DstarEx->set_params({gPsiDstarDstar, gDstarNLam, 0.});
+    psiDstar_DstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
+
+    auto psiDstar_Sum = new amplitude_sum(kpsiDstar, {psiDstar_DEx, psiDstar_DstarEx});
+
+    // ---------------------------------------------------------------------------
+    // Box Amplitude
 
     // Combine sub-processes in a box_loop
-    auto disc = new box_discontinuity(gamDDstarEx, psiDDstarEx);
-    auto dBox = new box_amplitude(kJPsi, disc, "Charm Loop");
+    auto dstarDisc = new box_discontinuity(gamDstar_Sum, psiDstar_Sum);
+    auto dstarBox  = new box_amplitude(kJPsi, dstarDisc, "D* Loop");
+    dstarBox->set_cutoff(s_cut(qmax, M_DSTAR));
 
-    auto W_cut = [&](double qmax)
-    { 
-        return sqrt(qmax*qmax + M2_LAMBDAC) + sqrt(qmax*qmax + M2_D);
-    };
+    // ---------------------------------------------------------------------------
+    // Combine both box loops
 
-    double qmax = 1.;
-    dBox->set_cutoff(W_cut(qmax) * W_cut(qmax));
-
-    double eta  = 1.;
-    gamDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
-    psiDDstarEx->set_formfactor(2, M_DSTAR + lambdaQCD * eta);
-
-    gamDDstarEx->set_debug(2);
-    psiDDstarEx->set_debug(2);
+    auto combined_box = new box_amplitude(kJPsi, {dDisc, dstarDisc}, "Sum");
 
     // ---------------------------------------------------------------------------
     // You shouldnt need to change anything below this line
     // ---------------------------------------------------------------------------
-    
+    std::vector<box_amplitude*> amps;
+    amps.push_back(dBox);
+    amps.push_back(dstarBox);
+    amps.push_back(combined_box);
+
     int N = 10;
 
    // ---------------------------------------------------------------------------
     // Print the desired observable for each amplitude
     jpacGraph1D * plotter = new jpacGraph1D();
 
-
     double xmin = E_beam(kJPsi->Wth()) + EPS;
-    double xmax = 9.2;
+    double xmax = 10.;
 
-    auto F = [&](double egam)
+    for (int n = 0; n < amps.size(); n++)
     {
-        double s = pow(W_cm(egam), 2.);
-        return dBox->integrated_xsection(s);
-    };
-    
-    auto x_fx = vec_fill(N, F, xmin, xmax, true);
-    plotter->AddEntry(std::get<0>(x_fx), std::get<1>(x_fx), "Du et al.");
+        std::cout << "\nPrinting amplitude: " << amps[n]->_identifier << "\n";
+        auto F = [&](double egam)
+        {
+            double s = pow(W_cm(egam), 2.);
+            return amps[n]->integrated_xsection(s);
+        };
+        
+        auto x_fx = vec_fill(N, F, xmin, xmax, true);
+        plotter->AddEntry(std::get<0>(x_fx), std::get<1>(x_fx), amps[n]->_identifier);
+    }
 
-    plotter->SetLegend(0.2, 0.6);
+    plotter->SetLegend(0.2, 0.3);
     plotter->SetLegendOffset(0.2, 0.1);
-    plotter->SetLegend(false);
 
-    plotter->SetYaxis("#sigma(#gamma p #rightarrow J/#psi p)    [nb]", 0., 1.);
+    plotter->SetYaxis("#sigma(#gamma p #rightarrow J/#psi  p)    [nb]", 0., 5.);
     plotter->SetXaxis("E_{#gamma}    [GeV]", xmin, xmax);
     plotter->Plot("helamp.pdf");
-
-    delete plotter;
-    delete kJPsi;
-    delete kgamD;
-    delete kpsiD;
-    delete disc;
-    delete dBox;
-    delete psiDDstarEx;
-    delete gamDDstarEx;
 
     return 1;
 };
