03a910a6df42c452080361614c625715cd663670
Daniel Winney <dwinney@iu.edu>
2022-04-11 22:55:58 +0800

    now plots nice comparison of all the relevant pi n xsections

--- executables/inclusive_XYZ/plot_sigmatot.cpp
+++ executables/inclusive_XYZ/plot_sigmatot.cpp
@@ -14,58 +14,81 @@ int main( int argc, char** argv )
     // Plotting options
     // ---------------------------------------------------------------------------
 
-    int N = 200;
+    int N = 500;
 
-    double  ymin = 1.;
-    double  ymax = 1.E3;
+    double  ymin = 7.;
+    double  ymax = 4.E2;
 
-    double xmin = (M_PION + M_PROTON) + EPS;   
-    double xmax = 5.;
+    double xmin = (M_PION + M_PROTON) + 0.1;   
+    double xmax = 3.;
 
     std::string filename = "sigma.pdf";
     std::string xlabel   = "W  [GeV]";
     std::string ylabel   = "#sigma_{tot}^{#pip}   [mb] ";
 
-    std::unique_ptr<total_xsection> sigma_tot_pimp( new PDG_parameterization(M_PION, M_PROTON, {-1., 1., 9.56, 1.767, 18.75}, "rpp2020-pimp_total.dat"));
-    std::unique_ptr<total_xsection> sigma_tot_pipp( new PDG_parameterization(M_PION, M_PROTON, {+1., 1., 9.56, 1.767, 18.75}, "rpp2020-pipp_total.dat"));
+    // std::unique_ptr<total_xsection> PDG_pimp(  new PDG_parameterization(M_PION, M_PROTON, {-1., 1., 9.56, 1.767, 18.75}));
+    // std::unique_ptr<total_xsection> PDG_pipp(  new PDG_parameterization(M_PION, M_PROTON, {+1., 1., 9.56, 1.767, 18.75}));
+
+    std::unique_ptr<total_xsection> JPAC_pimp_NR( new JPAC_parameterization(-1, false) );
+    std::unique_ptr<total_xsection> JPAC_pipp_NR( new JPAC_parameterization(+1, false) );
+
+    std::unique_ptr<total_xsection> JPAC_pimp_R( new JPAC_parameterization(-1, true) );
+    std::unique_ptr<total_xsection> JPAC_pipp_R( new JPAC_parameterization(+1, true) );
 
     // ---------------------------------------------------------------------------
     // You shouldnt need to change anything below this line
     // ---------------------------------------------------------------------------
 
     // Plotter object
-    auto plotter = new jpacGraph1D();
+    std::unique_ptr<jpacGraph1D> plotter( new jpacGraph1D() );
 
     // ---------------------------------------------------------------------------
     // Print the phase-space for each kinematics
 
+    std::array<std::vector<double>, 2> x_fx; 
+
     auto F = [&](double W)
     {
         double s = W*W;
-        return sigma_tot_pipp->eval(s);
+        return JPAC_pimp_R->eval(s);
     };
-
-    std::array<std::vector<double>, 2> x_fx; 
-    x_fx = vec_fill(N, F, xmin, xmax, true);
-    plotter->AddEntry(x_fx[0], x_fx[1], "#pi^{+} p");
+    x_fx = vec_fill(N, F, xmin, xmax);
+    plotter->AddEntry(x_fx[0], x_fx[1], "#pi^{-} p");
 
     x_fx[0].clear(); x_fx[1].clear();
-
     auto G = [&](double W)
     {
         double s = W*W;
-        return sigma_tot_pimp->eval(s);
+        return JPAC_pipp_R->eval(s);
     };
+    x_fx = vec_fill(N, G, xmin, xmax);
+    plotter->AddEntry(x_fx[0], x_fx[1], "#pi^{+} p");
+    
+    x_fx[0].clear(); x_fx[1].clear();
+    auto FF = [&](double W)
+    {
+        double s = W*W;
+        return JPAC_pimp_NR->eval(s);
+    };
+    x_fx = vec_fill(N, FF, xmin, xmax);
+    plotter->AddDashedEntry(x_fx[0], x_fx[1]);
 
-    x_fx = vec_fill(N, G, xmin, xmax, true);
+    x_fx[0].clear(); x_fx[1].clear();
 
-    plotter->AddEntry(x_fx[0], x_fx[1], "#pi^{-} p");
+    auto GG = [&](double W)
+    {
+        double s = W*W;
+        return JPAC_pipp_NR->eval(s);
+    };
+    x_fx = vec_fill(N, GG, xmin, xmax);
+    plotter->AddDashedEntry(x_fx[0], x_fx[1]);
 
+    // Axes and legend options
     plotter->SetXaxis(xlabel, 0., xmax);
     plotter->SetYaxis(ylabel, ymin, ymax);
     plotter->SetYlogscale(true);
 
-    plotter->SetLegend(0.7, 0.3);
+    plotter->SetLegend(0.7, 0.2);
     plotter->SetLegendOffset(0.3, 0.075);
 
     // Output to file
