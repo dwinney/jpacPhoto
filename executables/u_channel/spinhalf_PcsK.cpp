6031558526a4136705b35f65269a7514b85cd780
daniel <dwinney@iu.edu>
2022-05-30 16:14:41 +0800

    integrated cross-section from lambda exchange working

--- executables/u_channel/spinhalf_PcsK.cpp
+++ executables/u_channel/spinhalf_PcsK.cpp
@@ -1,6 +1,7 @@
 #include "constants.hpp"
 #include "reaction_kinematics.hpp"
 #include "amplitudes/dirac_exchange.hpp"
+#include "amplitudes/vector_exchange.hpp"
 
 #include "jpacGraph1D.hpp"
 #include "jpacUtils.hpp"
@@ -14,12 +15,12 @@ int main( int argc, char** argv )
     // Set up kinematics of the Pcs K final state
     double M_PCS = 4.5880;
     reaction_kinematics * kPcsK = new reaction_kinematics(M_KAON, M_PCS);
-    kPcsK->set_meson_JP(PSEUDO_SCALAR);
-    kPcsK->set_baryon_JP(HALF_MINUS);
+    kPcsK->set_meson_JP(  PSEUDO_SCALAR );
+    kPcsK->set_baryon_JP( HALF_MINUS    );
 
     // Couplings 
-    double gPhoton  =  5.10E-3;
-    double gNucleon = -6.44; 
+    double gPhoton  =  0.0412501;
+    double gNucleon = -6.4469; 
     double cutoff   = 0.9;
 
     // Create the amplitude
@@ -31,18 +32,10 @@ int main( int argc, char** argv )
 
     int   N = 50;
     bool PRINT_TO_COMMANDLINE  = true;
-
-    // ---------------------------------------------------------------------------
-    double  xmin = 5.;
-    double  xmax = 12.;
-
-    double  ymin = 0.;
-    double  ymax = 0.2;
-    // ---------------------------------------------------------------------------
-
-    std::string ylabel    = "#sigma(#gamma p #rightarrow P_{cs} K)  [pb]";
-    std::string xlabel    = "W_{#gammap}  [GeV]";
     std::string filename  = "PcsK.pdf";
+
+    // ---------------------------------------------------------------------------
+
 
     // ---------------------------------------------------------------------------
     // You shouldnt need to change anything below this line
@@ -52,16 +45,24 @@ int main( int argc, char** argv )
     jpacGraph1D* plotter = new jpacGraph1D();
 
     // ---------------------------------------------------------------------------
-    // Print the desired observable for each amplitude
+    double  xmin = 5.;
+    double  xmax = 12.;
+
+    double  ymin = 0.;
+    double  ymax = 0.8;
+
+    std::string ylabel    = "#sigma(#gamma p #rightarrow P K)  [nb]";
+    std::string xlabel    = "W_{#gammap}  [GeV]";
+
     auto F = [&](double x)
     {
-        return LamEx.integrated_xsection(x*x) * 1.E3;
+        return LamEx.integrated_xsection(x*x);
     };
 
     plotter->AddEntry(N, F, {xmin,xmax}, LamEx.get_id(), PRINT_TO_COMMANDLINE);
 
     plotter->SetXaxis(xlabel, xmin, xmax);
-    plotter->SetYaxis(ylabel/* , ymin, ymax */);
+    plotter->SetYaxis(ylabel,  ymin, ymax);
     plotter->SetLegend(0.7, 0.65);
 
     // Output to file
