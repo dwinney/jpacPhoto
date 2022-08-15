5a0b4509f60ff78ff6480ea5edd269138fe6c1df
daniel <dwinney@iu.edu>
2022-08-15 23:27:35 +0800

    new exes for gamma p -> D Lam all set

---
+++ examples/box/gamp_dlam.cpp
@@ -0,0 +1,107 @@
+// ---------------------------------------------------------------------------
+// Total photoproduction cross-sections of the Lambda_c D final state
+// Amplitude constructed from considering open-charm exchanges in t and u channels
+// 
+// Author:       Daniel Winney (2022)
+// Affiliation:  Joint Physics Analysis Center (JPAC)
+// Email:        dwinney@iu.edu
+// ---------------------------------------------------------------------------
+// References:
+// [1] arXiv:2009.08345v1
+// ---------------------------------------------------------------------------
+
+#include "constants.hpp"
+#include "reaction_kinematics.hpp"
+#include "pseudoscalar_exchange.hpp"
+#include "vector_exchange.hpp"
+#include "dirac_exchange.hpp"
+#include "amplitude_sum.hpp"
+
+#include "jpacGraph1D.hpp"
+
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+
+using namespace jpacPhoto;
+
+void gamp_dlam()
+{
+    // Form factor parameter
+    double eta = 1.;
+    double lambdaQCD = 0.25;
+
+    // ---------------------------------------------------------------------------
+    // D phototproduction
+    // ---------------------------------------------------------------------------
+
+    // Set up Kinematics for Dbar LambdaC in final state
+    reaction_kinematics kD (M_D, M_LAMBDAC);
+    kD.set_meson_JP(0, -1);
+
+    vector_exchange d_dstarEx (&kD, M_DSTAR, "D^{*} exchange");
+    d_dstarEx.set_params({0.134, -13.2, 0.});
+    d_dstarEx.set_formfactor(2, M_DSTAR + eta * lambdaQCD);
+    d_dstarEx.force_covariant(true);
+
+    dirac_exchange d_lamcEx (&kD, M_LAMBDAC, "#Lambda_{c} exchange");
+    d_lamcEx.set_params({sqrt(4.* PI * ALPHA), -4.3});
+    d_lamcEx.set_formfactor(2, M_LAMBDAC + eta * lambdaQCD);
+    d_lamcEx.force_covariant(true);
+
+    amplitude_sum d_sum (&kD,  {&d_dstarEx, &d_lamcEx}, "Sum");
+
+    // ---------------------------------------------------------------------------
+    // Plotting options
+    // ---------------------------------------------------------------------------
+
+    // which amps to plot
+    std::vector<amplitude*> amps;
+
+    amps.push_back(&d_dstarEx);
+    amps.push_back(&d_lamcEx);
+    amps.push_back(&d_sum);
+
+    int N = 50;
+    double PRINT = true;
+
+    double xmin = 4.;
+    double xmax = 5.;
+
+    double ymin = 0.;
+    double ymax = 550.;
+
+    std::string filename  = "open_charm.pdf";
+    std::string ylabel    = "#sigma(#gamma #it{p} #rightarrow #bar{#it{D}} #Lambda_{c}^{+})   [nb]";
+    std::string xlabel    = "#it{W}  [GeV]";
+
+    // ---------------------------------------------------------------------------
+    // You shouldnt need to change anything below this line
+    // ---------------------------------------------------------------------------
+
+    jpacGraph1D * plotter = new jpacGraph1D();
+
+    // ---------------------------------------------------------------------------
+    // Print the desired observable for each amplitude
+    for (int n = 0; n < amps.size(); n++)
+    {
+        auto F = [&](double w)
+        {
+            return amps[n]->integrated_xsection(w*w);
+        };
+
+        plotter->AddEntry(N, F, {xmin, xmax}, amps[n]->get_id(), PRINT);
+    };
+
+    plotter->SetXaxis(xlabel, xmin, xmax);
+    plotter->SetYaxis(ylabel, ymin, ymax);
+    plotter->SetLegend(0.2, 0.75);
+    plotter->SetLegendOffset(0.5, 0.1);
+
+    // Output to file
+    plotter->Plot(filename);
+
+    delete plotter;
+
+    return 0;
+};
--- examples/box/gamp_dlam_PWA.cpp
+++ examples/box/gamp_dlam_PWA.cpp
@@ -1,22 +1,21 @@
 // ---------------------------------------------------------------------------
-// Photoproduction cross-sections of the Lambda_c Dbar/D* final state
-// by open charm exchanges 
+// Photoproduction cross-sections of the Lambda_c D final state
+// Constructed by considering individual s-channel partial wave projections
 //
-// Author:       Daniel Winney (2020)
+// Here we consider up to J = 5/2
+//
+// Author:       Daniel Winney (2022)
 // Affiliation:  Joint Physics Analysis Center (JPAC)
-// Email:        dwinney@iu.edu
-// ---------------------------------------------------------------------------
-// References:
-// [1] arXiv:2009.08345v1
+// Email:        daniel.winney@gmail.edu
 // ---------------------------------------------------------------------------
 
 #include "constants.hpp"
 #include "reaction_kinematics.hpp"
-#include "amplitudes/pseudoscalar_exchange.hpp"
-#include "amplitudes/vector_exchange.hpp"
-#include "amplitudes/dirac_exchange.hpp"
-#include "amplitudes/amplitude_sum.hpp"
-
+#include "pseudoscalar_exchange.hpp"
+#include "vector_exchange.hpp"
+#include "dirac_exchange.hpp"
+#include "amplitude_sum.hpp"
+#include "projected_amplitude.hpp"
 #include "jpacGraph1D.hpp"
 
 #include <cstring>
@@ -25,12 +24,11 @@
 
 using namespace jpacPhoto;
 
-int main( int argc, char** argv )
+void gamp_dlam_PWA()
 {
     // Form factor parameter
     double eta = 1.;
     double lambdaQCD = 0.25;
-    bool du_result = false;
 
     // ---------------------------------------------------------------------------
     // D phototproduction
@@ -43,43 +41,25 @@ int main( int argc, char** argv )
     vector_exchange d_dstarEx (&kD, M_DSTAR, "D^{*} exchange");
     d_dstarEx.set_params({0.134, -13.2, 0.});
     d_dstarEx.set_formfactor(2, M_DSTAR + eta * lambdaQCD);
-    d_dstarEx.set_debug(1);
+    d_dstarEx.force_covariant(true);
 
     dirac_exchange d_lamcEx (&kD, M_LAMBDAC, "#Lambda_{c} exchange");
-    d_lamcEx.set_params({sqrt(4.* PI * ALPHA), -4.3});
+    d_lamcEx.set_params({sqrt(4.* PI * ALPHA), -4.3, 0.});
     d_lamcEx.set_formfactor(2, M_LAMBDAC + eta * lambdaQCD);
+    d_lamcEx.force_covariant(true);
 
-    amplitude_sum d_sum (&kD,  {&d_dstarEx, &d_lamcEx}, "Sum");
+    amplitude_sum d_sum (&kD,  {&d_dstarEx, &d_lamcEx}, "Full Sum");
 
     // ---------------------------------------------------------------------------
-    // D* phototproduction
+    // PW projection
     // ---------------------------------------------------------------------------
-
-    // Set up Kinematics for D* LambdaC in final state
-    reaction_kinematics kDstar (M_DSTAR, M_LAMBDAC);
-    kDstar.set_meson_JP(1, -1);
-
-    pseudoscalar_exchange dstar_dEx (&kDstar, M_D, "D exchange");
-    dstar_dEx.set_params({0.134, -4.3});
-    dstar_dEx.set_formfactor(2, M_D + eta * 0.250);
-    dstar_dEx.set_debug(1);
     
-    vector_exchange dstar_dstarEx (&kDstar, M_DSTAR, "D^{*} exchange");
-    dstar_dstarEx.set_params({0.641, -13.2, 0.});
-    dstar_dstarEx.set_formfactor(2, M_DSTAR + eta * 0.250);
-    dstar_dstarEx.set_debug(1);
+    // Take the sum ampitude and pass it to a projected_amplitude
+    projected_amplitude d_sum1(&d_sum, 1, "#it{J} = 1/2");
+    projected_amplitude d_sum3(&d_sum, 3, "#it{J} = 3/2");
+    projected_amplitude d_sum5(&d_sum, 5, "#it{J} = 5/2");
 
-    dirac_exchange dstar_lamcEx (&kDstar, M_LAMBDAC, "#Lambda_{c} exchange");
-    dstar_lamcEx.set_params({sqrt(4.* PI * ALPHA), -13.2});
-    dstar_lamcEx.set_formfactor(2, M_LAMBDAC + eta * 0.250);
-    
-    amplitude_sum dstar_sum (&kDstar, {&dstar_dEx, &dstar_dstarEx, &dstar_lamcEx}, "D^{*} production");
-
-    if (du_result)
-    {
-        dstar_dstarEx.set_debug(2);
-        d_dstarEx.set_debug(2);
-    }
+    amplitude_sum d_sum135 (&kD,  {&d_sum1, &d_sum3, &d_sum5}, "Sum up to #it{J}_{max} = 5/2");
 
     // ---------------------------------------------------------------------------
     // Plotting options
@@ -87,27 +67,24 @@ int main( int argc, char** argv )
 
     // which amps to plot
     std::vector<amplitude*> amps;
-
-    // amps.push_back(d_dstarEx);
-    // amps.push_back(d_lamcEx);
-    // amps.push_back(dstar_dEx);
-    // amps.push_back(dstar_dstarEx);
-    // amps.push_back(dstar_lamcEx);
+    amps.push_back(&d_sum1);
+    amps.push_back(&d_sum3);
+    amps.push_back(&d_sum5);
+    amps.push_back(&d_sum135);
     amps.push_back(&d_sum);
-    amps.push_back(&dstar_sum);
 
     int N = 50;
     double PRINT = true;
 
-    double xmin = 8.3;
-    double xmax = 10.5;
+    double xmin = 4.;
+    double xmax = 6.;
 
     double ymin = 0.;
-    double ymax = 400.;
+    double ymax = 1.;
 
     std::string filename  = "open_charm.pdf";
-    std::string ylabel    = "#sigma(#gamma p #rightarrow D^{(*)} #Lambda_{c}^{+})   [nb]";
-    std::string xlabel    = "E_{#gamma}  [GeV]";
+    std::string ylabel    = "#sigma(#gamma #it{p} #rightarrow #bar{#it{D}} #Lambda_{c}^{+})   [#mub]";
+    std::string xlabel    = "#it{W}  [GeV]";
 
     // ---------------------------------------------------------------------------
     // You shouldnt need to change anything below this line
@@ -119,10 +96,9 @@ int main( int argc, char** argv )
     // Print the desired observable for each amplitude
     for (int n = 0; n < amps.size(); n++)
     {
-        auto F = [&](double e)
+        auto F = [&](double w)
         {
-            double w = W_cm(e);
-            return amps[n]->integrated_xsection(w*w);
+            return amps[n]->integrated_xsection(w*w) * 1E-3;
         };
 
         plotter->AddEntry(N, F, {xmin, xmax}, amps[n]->get_id(), PRINT);
@@ -130,8 +106,8 @@ int main( int argc, char** argv )
 
     plotter->SetXaxis(xlabel, xmin, xmax);
     plotter->SetYaxis(ylabel, ymin, ymax);
-    plotter->SetLegend(0.2, 0.75);
-    plotter->SetLegendOffset(0.5, 0.1);
+    plotter->SetLegend(0.2, 0.65);
+    plotter->SetLegendOffset(0.5, 0.17);
 
     // Output to file
     plotter->Plot(filename);
