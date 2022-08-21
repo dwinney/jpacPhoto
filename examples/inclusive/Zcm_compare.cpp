0f245b792423625aa486cb42a7394759812739a4
daniel <dwinney@iu.edu>
2022-08-21 16:31:56 +0800

    added new plots for inclusive draft

--- examples/inclusive/Zcm_compare.cpp
+++ examples/inclusive/Zcm_compare.cpp
@@ -9,7 +9,8 @@
 #include <cstring>
 
 using namespace jpacPhoto;
-void Z_pT()
+
+void Zcm_compare()
 {
     // ---------------------------------------------------------------------------
     // Amplitudes
@@ -20,76 +21,84 @@ void Z_pT()
 
     // Couplings
     double g_NN = sqrt(2.) * sqrt(4. * PI * 13.81); // Nucleon coupling same for all
-    double g_delta = 18.5; // Dleta p pi coupling
     double LamPi = .9;  // 900 MeV cutoff for formfactor
 
     // Masses
     double M_ZC  = 3.8884; // GeV
+    double M_ZB  = 10.6072;
+    double M_ZBP = 10.6522;
 
     // ---------------------------------------------------------------------------
     // Zc(3900)
 
     // Kinematics
     reaction_kinematics kZc (M_ZC);
-    kZc.set_meson_JP(1, +1);
+    kZc.set_meson_JP(1, 1);
 
     // Coupling
     double gc_Psi = 1.91; // psi coupling before VMD scaling
     double gc_Gamma = E * F_JPSI * gc_Psi / M_JPSI;
 
     // Exclusive amplitude
-    pseudoscalar_exchange excZc (&kZc, M_PION, "inclusive Z_{c}");
+    pseudoscalar_exchange excZc (&kZc, M_PION, "total #it{Z}_{#it{c}}(3900)^{#minus} production");
     excZc.set_params({gc_Gamma, g_NN});
     excZc.set_formfactor(true, LamPi);
 
     // We now can pass this to an inclusive amplitude
     triple_regge incZc (&excZc);
     incZc.set_high_energy_approximation(false);
-    incZc.set_sigma_total(JPAC_pipp_withResonances);
 
     // ---------------------------------------------------------------------------
     // Plotting options
     // ---------------------------------------------------------------------------
 
     int N = 100;
-    double w = 6.;
 
-    incZc._kinematics->_s = w*w;
-    double pmax = incZc._kinematics->pMax();
+    double xmin = 4.8;   
+    double xmax = 10.;
 
-    double xmin = 0.01;   
-    double xmax = pmax;
+    double ymin = 0;
+    double ymax = 45.;
 
-    double ymin = 2.E-5;
-    double ymax = 50;
-
-    std::string filename = "incZ_qT.pdf";
-    std::string ylabel   = "d#sigma/d#it{q}_{T}     [nb / GeV]";
-    std::string xlabel   = "#it{q}_{T}    [GeV]";
+    std::string filename = "incZminus_compare.pdf";
+    std::string ylabel   = "#sigma  [nb]";
+    std::string xlabel   = "#it{W}_{#gamma#it{p}}  [GeV]";
     bool print_to_CMD    = true;
 
     // ---------------------------------------------------------------------------  
     // You shouldnt need to change anything below this line
     // ---------------------------------------------------------------------------
-
+ 
     // Plotter object
     jpacGraph1D * plotter = new jpacGraph1D();
-    
-    // ---------------------------------------------------------------------------
-    // Intermediate functions needed
 
-    // Just the inclusive curve
-    auto F = [&](double pT)
+    auto F = [&](double w)
     {
-        double y = pT / pmax;
-        return incZc.dsigma_dy(w*w, y) * 1.E3; // in nb!
+        return incZc.integrated_xsection(w*w) * 1.E3; // in nb!
     };
 
-    incZc.set_sigma_total(JPAC_pipp_withResonances);
-    plotter->AddEntry(N, F, {xmin, xmax}, "#it{Z}_{c}(3900)^{#minus}", print_to_CMD);
+    auto G = [&](double w)
+    {
+        return incZc.exclusive_xsection(w*w); // in nb!
+    };
+
+    auto H = [&](double w)
+    {
+        return G(w) + F(w); // in nb!
+    };
+
+    // Exlclusive only
+    plotter->AddEntry(N, G, {xmin, xmax}, "#it{Z}_{c}(3900)^{#minus} #it{n}", print_to_CMD);
+
+    incZc.set_sigma_total(JPAC_pimp_onlyDelta);
+    plotter->AddEntry(N, F, {xmin, xmax}, "#it{Z}_{c}(3900)^{#minus} (#Delta^{0} #rightarrow #pi^{#minus}#it{p})", print_to_CMD);
 
     incZc.set_sigma_total(JPAC_pimp_withResonances);
-    plotter->AddEntry(N, F, {xmin, xmax}, "#it{Z}_{c}(3900)^{#plus}", print_to_CMD);
+    plotter->AddEntry(N, H, {xmin, xmax}, "Total", print_to_CMD);
+
+    incZc.set_sigma_total(JPAC_pimp_onlyDelta);
+    plotter->AddDashedEntry(N, H, {xmin, xmax}, print_to_CMD);
+    
 
     // ---------------------------------------------------------------------------
     // Finally make the plot pretty
@@ -97,16 +106,11 @@ void Z_pT()
     // Axes options
     plotter->SetXaxis(xlabel, xmin, xmax);
     plotter->SetYaxis(ylabel, ymin, ymax);
-    plotter->SetYlogscale(true);
-    plotter->SetXlogscale(true);
-
-    std::ostringstream streamObj;
-    streamObj << std::setprecision(4) << "#it{W}_{#gammap} = " << w << " GeV";
-    std::string header = streamObj.str();
+    // plotter->SetYlogscale(true);
 
     // LEgend options
-    plotter->SetLegend(0.35, 0.35, header);
-    plotter->SetLegendOffset(0.3, 0.12);
+    plotter->SetLegend(0.23, 0.75);
+    plotter->SetLegendOffset(0.3, 0.15);
 
     // Output to file
     plotter->Plot(filename);
---
+++ examples/inclusive/b1_average.cpp
@@ -0,0 +1,94 @@
+#include "triple_regge.hpp"
+#include "inclusive_kinematics.hpp"
+#include "regge_trajectory.hpp"
+
+#include "Math/GSLIntegrator.h"
+#include "Math/IntegrationTypes.h"
+#include "Math/Functor.h"
+
+#include "jpacUtils.hpp"
+#include "jpacGraph1D.hpp"
+
+#include <iostream>
+#include <cstring>
+
+using namespace jpacPhoto;
+
+void b1_average()
+{
+    // ---------------------------------------------------------------------------
+    // Data from OmegaPHOTON 
+    // ---------------------------------------------------------------------------
+    
+    double s = 75.9421;
+    std::vector<double> dat_x = {0.65, 0.75, 0.85, 0.95};
+    std::vector<double> err_x = {0.05, 0.05, 0.05, 0.05};
+
+    std::vector<double> dat_sigma = {1.80957, 2.15690, 1.3661, 0.65901};
+    std::vector<double> err_sigma = {2.36188 - 1.80957, 2.47490 - 2.15690, 1.53345 - 1.36611, 0.76779 - 0.65901};
+
+    // ---------------------------------------------------------------------------
+    // Amplitudes
+    // ---------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------
+    // Global quantities
+
+    // Couplings
+    double g_b1 = 0.24;
+    double g_NN = sqrt(2.) * sqrt(4. * PI * 13.81); // Nucleon coupling same for all
+    double LamPi = .9;  // 900 MeV cutoff for formfactor
+    
+    // Kinematics
+    reaction_kinematics kb1 (M_B1);
+    kb1.set_meson_JP(1, +1);
+
+    // ---------------------------------------------------------------------------
+    // Reggeized pion amplitude 
+
+    // Pion trajectory 
+    int signature = +1;
+    double alpha_prime = 0.7; // GeV^-2
+    double alpha_0 =  - alpha_prime * M2_PION;
+    linear_trajectory alpha (signature, alpha_0, alpha_prime);
+    alpha.set_minJ(0);
+
+    // Exclusive amplitude
+    pseudoscalar_exchange excB1r (&kb1, &alpha, "b1 production");
+    excB1r.set_params({g_b1, g_NN});
+    excB1r.set_formfactor(true, LamPi);
+
+    // We now can pass this to an inclusive amplitude
+    triple_regge incB1r (&excB1r);
+    incB1r.set_high_energy_approximation(true);
+
+    // ---------------------------------------------------------------------------
+    // Plotting options
+    // ---------------------------------------------------------------------------
+
+    auto F = [&](double x)
+    {
+        return incB1r.dsigma_dx(s, x) * 1.E3; // in mub!
+    };
+
+    auto G = [&](double xmin, double xmax)
+    {
+        ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
+        ROOT::Math::Functor1D wF(F);
+        ig.SetFunction(wF);
+
+        return ig.Integral(xmin, xmax) / (xmax - xmin);
+    };
+
+    incB1r.set_sigma_total(JPAC_pimp_withResonances);
+    double sig_res = G(0.9, 1);
+
+    incB1r.set_sigma_total(PDG_pimp_onlyRegge);
+    double sig_reg = G(0.9, 1);
+
+
+    debug("Data", "0.659 +- 0.11", "mub");
+    debug("w/ Resonances", sig_res, "mub");
+    debug("only Regge", sig_reg, "mub");
+
+};
