d0b11954e12254f4a74187930108f6f3e8da3f5d
daniel <dwinney@iu.edu>
2022-09-05 00:09:02 +0800

    added all differential cross-sections

--- src/inclusive/inclusive_production.cpp
+++ src/inclusive/inclusive_production.cpp
@@ -10,6 +10,51 @@
 #include "inclusive_production.hpp"
 
 // ---------------------------------------------------------------------------
+// Differential cross-sections
+// OUTPUT IN NANOBARN
+
+
+double jpacPhoto::inclusive_production::dsigma_dtdM2(double s, double t, double M2)
+{
+    if (sqrt(s) <= _kinematics->_mX + _kinematics->_mT) return 0.;
+
+    // Pass the total energy to the kinematics object
+    _kinematics->_s = s; 
+
+    double mx;
+    (_useTX) ? (mx = _kinematics->XfromTM2(t, M2)) : (mx = M2); 
+
+    return _kinematics->jacobianTM2(t, M2) * d3sigma_d3p(s, t, mx);
+}
+
+double jpacPhoto::inclusive_production::dsigma_dtdx(double s, double t, double x)
+{
+    if (sqrt(s) <= _kinematics->_mX + _kinematics->_mT) return 0.;
+
+    // Pass the total energy to the kinematics object
+    _kinematics->_s = s; 
+
+    double mx;
+    (_useTX) ? (mx = x) : (mx = _kinematics->M2fromTX(t, x)); 
+
+    return _kinematics->jacobianTX(t, x) * d3sigma_d3p(s, t, mx);
+}
+
+double jpacPhoto::inclusive_production::dsigma_dxdy2(double s, double x, double y2)
+{
+    if (sqrt(s) <= _kinematics->_mX + _kinematics->_mT) return 0.;
+
+    // Pass the total energy to the kinematics object
+    _kinematics->_s = s; 
+
+    double t = _kinematics->TfromXY2(x ,y2);
+    double mx;
+    (_useTX) ? (mx = x) : (mx = _kinematics->M2fromXY2(t, x)); 
+
+    return _kinematics->jacobianXY2(t, x) * d3sigma_d3p(s, t, mx);
+}
+
+// ---------------------------------------------------------------------------
 // Singly-differential cross-sections in terms of (t, M2)
 double jpacPhoto::inclusive_production::dsigma_dt(double s, double t)
 {
@@ -22,23 +67,15 @@ double jpacPhoto::inclusive_production::dsigma_dt(double s, double t)
     double result = 0.;
     ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
 
-    if (_useTX == false)
+    // Integrate the differential cross-section
+    auto dSigma = [&](double M2)
     {
-        // Assume argument 3 is M2
-        auto dSigma = [&](double M2)
-        {
-            return _kinematics->jacobianTM2(t, M2) * d3sigma_d3p(s, t, M2);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
-
-        result = ig.Integral(_kinematics->M2MINfromT(t), _kinematics->M2MAXfromT(t));
-    }
-    else
-    {
-        // TODO: add integration over x at fixed t
-        result = 0.;
+        return dsigma_dtdM2(s, t, M2);
     };
+    ROOT::Math::Functor1D wF(dSigma);
+    ig.SetFunction(wF);
+
+    result = ig.Integral(_kinematics->M2MINfromT(t), _kinematics->M2MAXfromT(t));
 
     return result;
 };
@@ -55,34 +92,15 @@ double jpacPhoto::inclusive_production::dsigma_dM2(double s, double M2)
     double result = 0.;
     ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
 
-    if (_useTX == false)
+    // integrate over usual t bounds
+    auto dSigma = [&](double t)
     {
-        // Assume argument 3 is M2
-        // integrate over usual t bounds
-        auto dSigma = [&](double t)
-        {
-            return _kinematics->jacobianTM2(t, M2) * d3sigma_d3p(s, t, M2);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
-
-        result = ig.Integral(_kinematics->TMAXfromM2(M2), _kinematics->TMINfromM2(M2));
-    }
-    else
-    {
-        // Argument 3 is x
-        // Convert the M2 taken as argument to an x in the high-energy limit
-        double x = 1. - M2 / s;
-
-        auto dSigma = [&](double t)
-        {
-            return _kinematics->jacobianTX(t, x) * d3sigma_d3p(s, t, x);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
-
-        result = ig.Integral(_kinematics->TMINfromX(x), _kinematics->TMAXfromX(x));
+        return _kinematics->jacobianTM2(t, M2) * d3sigma_d3p(s, t, M2);
     };
+    ROOT::Math::Functor1D wF(dSigma);
+    ig.SetFunction(wF);
+
+    result = ig.Integral(_kinematics->TMAXfromM2(M2), _kinematics->TMINfromM2(M2));
 
     return result;
 };
@@ -100,34 +118,15 @@ double jpacPhoto::inclusive_production::dsigma_dy2(double s, double y2)
     double result = 0.;
     ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
 
-    if (_useTX == false)
+    // Argument 3 is x and we dont need to convert
+    auto dSigma = [&](double x)
     {
-        // Assume argument 3 is M2
-        auto dSigma = [&](double x)
-        {
-            // convert x and y2 to fixed M2
-            double M2 = _kinematics->M2fromXY2(x, y2);
-            double t  = _kinematics->TfromXY2(x, y2);
-            return _kinematics->jacobianXY2(t, M2) * d3sigma_d3p(s, t, M2);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
-
-        result = ig.Integral(0., sqrt(1. - y2));
-    }
-    else
-    {
-        // Argument 3 is x and we dont need to convert
-        auto dSigma = [&](double x)
-        {
-            double t  = _kinematics->TfromXY2(x, y2);
-            return _kinematics->jacobianXY2(x, y2) * d3sigma_d3p(s, t, x);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
-
-        result = ig.Integral(0., sqrt(1. - y2));
+        return dsigma_dxdy2(s, x, y2);
     };
+    ROOT::Math::Functor1D wF(dSigma);
+    ig.SetFunction(wF);
+
+    result = ig.Integral(0., sqrt(1. - y2));
 
     return result;
 };
@@ -143,40 +142,22 @@ double jpacPhoto::inclusive_production::dsigma_dx(double s, double x)
     // How we integrate depends on what variables are being used
     double result = 0.;
     ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
-    
-    if (_useTX == false)
-    {
-        // Assume argument 3 is M2
-        auto dSigma = [&](double y2)
-        {
-            // convert x and y2 to fixed M2
-            double M2 = _kinematics->M2fromXY2(x, y2);
-            double t  = _kinematics->TfromXY2(x, y2);
-            return _kinematics->jacobianXY2(x, y2) * d3sigma_d3p(s, t, M2);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
 
-        result = ig.Integral(0., 1. - x*x);
-    }
-    else
+    // Argument 3 is x and we dont need to convert
+    auto dSigma = [&](double y2)
     {
-        // Argument 3 is x and we dont need to convert
-        auto dSigma = [&](double y2)
-        {
-            double t  = _kinematics->TfromXY2(x, y2);
-            return _kinematics->jacobianXY2(x, y2) * d3sigma_d3p(s, t, x);
-        };
-        ROOT::Math::Functor1D wF(dSigma);
-        ig.SetFunction(wF);
-
-        result = ig.Integral(0., 1. - x*x);
+        return dsigma_dxdy2(s, x, y2);
     };
+    ROOT::Math::Functor1D wF(dSigma);
+    ig.SetFunction(wF);
 
+    result = ig.Integral(0., 1. - x*x);
+   
     return result;
 };
 
 // ---------------------------------------------------------------------------
+// OUTPUT IN NANOBARN
 double jpacPhoto::inclusive_production::integrated_xsection(double s)
 {
     if (sqrt(s) <= _kinematics->_mX + sqrt(_kinematics->_minM2)) return 0.;
@@ -193,22 +174,20 @@ double jpacPhoto::inclusive_production::integrated_xsection(double s)
         // Assume argument 3 is M2
         auto dSigma = [&](double x)
         {
-            double sigma = dsigma_dx(s,x);
-            return sigma;
+            return dsigma_dx(s,x);
         };
         ROOT::Math::Functor1D wF(dSigma);
         ig.SetFunction(wF);
 
         // Integrate over x
-        result  = ig.Integral(0.1, 1.);
+        result  = ig.Integral(0., 1.);
     }
     else
     {
         // Assume argument 3 is M2
         auto dSigma = [&](double m2)
         {
-            double sigma = dsigma_dM2(s,m2);
-            return sigma;
+            return dsigma_dM2(s, m2);
         };
         ROOT::Math::Functor1D wF(dSigma);
         ig.SetFunction(wF);
@@ -217,5 +196,5 @@ double jpacPhoto::inclusive_production::integrated_xsection(double s)
         result = ig.Integral(_kinematics->_minM2, pow(sqrt(s) - _kinematics->_mX, 2));
     }
 
-    return result * 1.E3;
+    return result;
 };
